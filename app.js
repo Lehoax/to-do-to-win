4f40afb92c7360b22a4a879d4ad913e49ffb5962
Leo <leolair.pro@gmail.com>
2024-10-14 17:32:50 +0200

    init

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+mongoose.set('strictQuery', false);
 
 const users_routes = require('./routes/user')
 const authenticateToken = require('./middleware/auth');
