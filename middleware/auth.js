794755bbf457f83fce957ac5c603ce4de8c4a554
Leo <leolair.pro@gmail.com>
2024-10-22 15:49:57 +0200

    updtate done task

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 function authenticateToken(req, res, next) {  
-    const authHeader = req.headers['authorization'];
+    const authHeader = req.headers['authorization'];    
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) {
       return res.sendStatus(401);
